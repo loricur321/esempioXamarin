740c60e282316da4ea63d1028d6896ed683bf713
loricur321 <loricur@gmail.com>
2021-01-20 10:41:47 +0100

    Primo commit

---
+++ esempioXamarin/esempioXamarin/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using esempioXamarin.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace esempioXamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            //programma orari autobus
+        }
+
+        private void btnCalcola_Clicked(object sender, EventArgs e)
+        {
+            Orario orarioPartenzaPennabilli160 = new Orario(6, 13); //creo i due orari
+            Orario orarioArrivoCentroStudi160 = new Orario(7, 42);
+
+            int delta = orarioPartenzaPennabilli160.DeltaOrario(orarioArrivoCentroStudi160); //tramite la classe calcolo la differenza tra i due orari
+
+            lblRisultato.Text = $"{delta.ToString()} minuti"; //e tramite la label stampo una stringa
+        }
+    }
+}
---
+++ esempioXamarin/esempioXamarin/Models/Orario.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace esempioXamarin.Models
+{
+    class Orario
+    {
+        public int ore;
+        public int minuti;
+
+
+        public Orario() //costruttore di default
+        {
+            ore = 0;
+            minuti = 0;
+        }
+
+
+        public Orario(int o, int m)//costruttore
+        {
+            ore = o;
+            minuti = m;
+        }
+
+        public int DeltaOrario(Orario o)//metodo che calcola la differnza in minuti tra due orari
+        {
+            int m1 = this.ToMinutes(); 
+            int m2 = o.ToMinutes();
+
+            return Math.Abs(m1-m2);//ritorno un valore assoluto per evitare che il risultato venga negativo
+        }
+
+        private int ToMinutes()//tramite questo metodo converto un'orario in minuti
+        {
+            return ore * 60 + minuti;
+        }
+
+        
+
+        
+        public string Stampa() //metodo che restituisce una stringa che verrà usata per stampare l'orario
+        {
+            return $"{this.ore} : {this.minuti}";
+        }
+    }
+
+}
